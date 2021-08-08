6b3f46efaafadcec1aec02c3a5e22519acd85b28
MAURO SERRANO <maurinholex10@gmail.com>
2021-08-07 23:07:13 -0300

    creacion de los modelos pokemon y tipo, y vinculo entre las dos tablas

---
+++ api/src/models/Tipo.js
@@ -0,0 +1,23 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('tipo', {
+     
+    id:  {
+        type: DataTypes.UUID,
+        primaryKey: true
+    },
+
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+
+    
+
+
+  });
+};
